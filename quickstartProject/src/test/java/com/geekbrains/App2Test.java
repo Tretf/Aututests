f2ee05fd9b26fb37fedc0a4600ca720c99feb615
r <rahira.s@mail.ru>
2022-01-22 18:59:16 +0500

    HW_Lesson2

---
+++ quickstartProject/src/test/java/com/geekbrains/App2Test.java
@@ -0,0 +1,27 @@
+package com.geekbrains;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App2Test.
+ */
+public class App2Test
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        System.out.println("App2Test");
+        assertTrue( true );
+    }
+
+    @Test
+    public void shouldAnswerWithTrue1() {
+        System.out.println("App1");
+        assertTrue(1 == 1);
+    }
+
+}
---
+++ quickstartProject/src/test/java/com/geekbrains/AppTest.java
@@ -0,0 +1,27 @@
+package com.geekbrains;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App2Test.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        System.out.println("AppTest");
+        assertTrue( true );
+    }
+
+    @Test
+    public void shouldAnswerWithTrue1() {
+        System.out.println("AppTest1");
+        assertTrue(1 == 2);
+    }
+
+}
